db07562bd3a41ac279b4ed2c9fc54e7c374ab8a1
Beloglazov, Alexander <abelogla@buffalo.edu>
2017-02-02 12:42:46 -0500

    Helpers.pm removed

--- KongApi/UserAgent.pm
+++ KongApi/UserAgent.pm
@@ -19,7 +19,6 @@ sub BUILDARGS {
 sub request {
     my ($self, %args) = @_;
     croak 'Unspecified type of the request' unless defined $args{type};
-    # print $args{path}."\n";
     $self->server->path($args{path});
     $self->server->query_form($args{querystring}) if exists $args{querystring};
     my $req = HTTP::Request->new($args{type}, $self->server->canonical);
